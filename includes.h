5c236728d1a64dd3aafca2065d010730d89722cf
cheatloverz <81638508+cheatloverz@users.noreply.github.com>
2021-04-15 23:50:07 +0000

    Update includes.h

--- includes.h
+++ includes.h
@@ -1,1 +1,10 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <time.h>
+#include <math.h>
 
+#include "scanner.h"
+#include "bypass.h
